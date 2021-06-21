cb32e3142974d92430028af6b3422098647df0b2
Jigar Dave <jdavenz8@gmail.com>
2021-06-21 16:09:17 +0530

    Initial Commit

---
+++ Slot Machine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slot Machine
+//
+//  Created by jigar dave on 21/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
